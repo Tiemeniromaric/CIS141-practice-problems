74de349407b1f46aa105da9ef848723f54992178
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-04-23 09:20:11 -0700

    Update problem5.py

--- Mod3/problem5.py
+++ Mod3/problem5.py
@@ -1,1 +1,9 @@
+'''
+Print 3 words with a | character (called a pipe) in between them.
+Use the appropriate keyword argument in print() to do so.
+'''
+variable1 = "Never give up!"
+variable2 = variable1.split(" ")
+variable3 = "|".join(variable2)
+print(variable3)
 
