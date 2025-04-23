df2de83e83e15ccaea72459045feff649e0d01fa
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-04-23 09:17:45 -0700

    Update problem3.py

--- Mod3/problem3.py
+++ Mod3/problem3.py
@@ -1,1 +1,8 @@
+'''
+Prompt the user for a sentence and a word to try to find in that sentence.
+Have the program print out whether the word was found in the sentence. (i.e. True or False)
+'''
+sentence = input("can you please type a sentence?")
+word_to_search = input ("can you please provide a word to search in your sentence?")
+print(word_to_search in sentence)
 
