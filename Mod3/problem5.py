dd48d3e1bb26bd7296200bd6e80298931693510c
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-04-24 13:00:54 -0700

    Update problem5.py
    I added the second way to solve the problem.

--- Mod3/problem5.py
+++ Mod3/problem5.py
@@ -2,8 +2,16 @@
 Print 3 words with a | character (called a pipe) in between them.
 Use the appropriate keyword argument in print() to do so.
 '''
+
+#Fisrt way to do it
 variable1 = "Never give up!"
 variable2 = variable1.split(" ")
 variable3 = "|".join(variable2)
 print(variable3)
 
+#Second way to do it
+var1 = "Never"
+var2 = "give"
+var3 = " up!"
+
+print(var1, var2, var3, sep="|")
