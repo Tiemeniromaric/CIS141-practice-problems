a2aa5c7c90708232b67203e96a928b68d589da62
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-04-29 14:42:05 -0700

    Update problem5.py

--- Mod4/problem5.py
+++ Mod4/problem5.py
@@ -1,1 +1,13 @@
+'''
+A store charges $5 for shipping on any order under $50. If the order amount is $50 or more, shipping is free.
+Ask the user for the order total and print the total cost, including shipping.
+'''
+
+order_amont = float( input("What is your total order amont ? : $"))
+
+if order_amont >= 50 :
+    print(f"Your tocal cost is $ {order_amont :.2f}.")
+    
+else:
+    print(f"your total cost is ${order_amont + 5:.2f}.")
 
