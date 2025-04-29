57f413756f5337a35402a376d7ff8e18591bc6aa
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-04-29 14:44:42 -0700

    Update problem1.py

--- Mod4/problem1.py
+++ Mod4/problem1.py
@@ -1,1 +1,23 @@
+'''
+Construct a truth table for the expression: (A AND B) OR (NOT B) where A and B each can be True or False.
+Your truth table should be commented out (as it's not valid Python code!)
+'''
+
+# 1. List variables
+# A,B
+
+# 2. Determine the number of Rows
+# 2 possibilities for A
+# 2 possibilities for B
+# 2 * 2 combinations of A&B = 4
+
+# 3. Create Table columns
+#A  B   (A AND B)   (A OR B)  (NOT B)
+
+# 5. Determine the number of Rows
+#A  B   (A AND B)   (A OR B)  (NOT B)
+#T  T       T           T        F
+#T  F       F           T        F
+#F  T       F           T        F
+#F  F       F           F        T
 
