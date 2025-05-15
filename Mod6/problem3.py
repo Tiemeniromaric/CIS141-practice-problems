e9eb1fc018b8026a8119ceb3a32a05d70f9a7f2b
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-05-14 21:37:56 -0700

    Create problem3.py

---
+++ Mod6/problem3.py
@@ -0,0 +1,12 @@
+'''
+ Create a list of strings. Write code to create a new list that includes only the strings longer than three characters.
+ Print the resulting filtered list.
+'''
+
+Cameroon_Cities= ['Yde','Buea','Dschang','Dla','Bamenda','Limbe','Bafang','Nde']  #Original list of the strings
+New_Cities = []                   #Empty list to store the filtered list
+for char in Cameroon_Cities:      #Loop in the lsit of the original list
+    if len(char)> 3:
+        New_Cities.append(char)
+print(New_Cities)
+
