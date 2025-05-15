4ba92907e963a54ef3b2eb3484766c44c40e18f1
Romaric Tiemeni <romarictchokouaha@student.olympic.edu>
2025-05-14 21:47:06 -0700

    Update problem2.py

--- Mod6/problem2.py
+++ Mod6/problem2.py
@@ -2,7 +2,6 @@
 Create a list of strings.
 Write code that counts how many times the word "Olympic" appears in the list, and then print the count.
 '''
-
 Community_College = ["Bellevue","Peninsula","Olympic","Seatle","Tacoma","Olympia","Olympic"]  #List of the strings
 count = 0                          #initial count
 for char in Community_College:     #Loop through the list of strings
